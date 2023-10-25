0dd8df340a46b1a545acd4dca5fbf9f69b42ec4d
Bruno T <32988099+brunotiglla@users.noreply.github.com>
2023-10-25 01:23:09 -0500

    test1

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import React, {useEffect, useState} from 'react'
-import Dictaphone  from './components/add.js';
-import AudioRecorder from './components/AudioRecorder.js';
+import React, { useState, useEffect } from 'react';
+import AudioRecorder from './components/AudioRecorder';
 
 function App() {
-
-  const [backenData, setBackendData] = useState(({}))
+  const [backendData, setBackendData] = useState({});
 
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)},
-    )
-  
-  }, [])
-  
-
+    fetch("/api")
+      .then(response => response.json())
+      .then(data => setBackendData(data));
+  }, []);
 
   return (
-    
-    
-
-
-
     <div>
-      {(typeof backenData.users === 'undefined') ? (
-      <p> loading...</p>):(
-        backenData.users.map((user,i) => (
-          <p key= {i}>{user}</p>
+      {(typeof backendData.users === 'undefined') ? (
+        <p> loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
         ))
       )}
 
-      <h1>Reconocimiento de Voz en App</h1>
-      <Dictaphone /> {/* Aquí estás utilizando el componente Dictaphone */}
-    
-      <div>
       <AudioRecorder />
-      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -1,56 +1,100 @@
+import React, { useRef } from "react";
+import toWav from 'audiobuffer-to-wav';
 
-import React, { useState } from 'react';
+const AudioRecorder = () => {
+  const mediaRecorder = useRef(null);
+  const audioChunks = useRef([]);
 
-function AudioRecorder() {
-  const [recording, setRecording] = useState(false);
-  const [audioChunks, setAudioChunks] = useState([]);
-  const [audioUrl, setAudioUrl] = useState(null);
-  let mediaRecorder;
 
   const startRecording = () => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
       .then((stream) => {
-        mediaRecorder = new MediaRecorder(stream);
-
-        mediaRecorder.ondataavailable = (event) => {
+        mediaRecorder.current = new MediaRecorder(stream);
+        mediaRecorder.current.ondataavailable = (event) => {
           if (event.data.size > 0) {
-            setAudioChunks([...audioChunks, event.data]);
+            audioChunks.current.push(event.data);
           }
         };
-
-        mediaRecorder.onstop = () => {
-          const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-          setAudioUrl(URL.createObjectURL(audioBlob));
-          setAudioChunks([]);
-        };
-
-        mediaRecorder.start();
-        setRecording(true);
+        mediaRecorder.current.start();
       })
       .catch((error) => {
-        console.error('Error al acceder al micrófono:', error);
+        console.error("Error starting recording:", error);
       });
   };
 
-  const stopRecording = () => {
-    if (mediaRecorder && recording) {
-      mediaRecorder.stop();
-      setRecording(false);
+  const stopRecordingAndDownload = async () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === "recording") {
+      mediaRecorder.current.stop();
+      mediaRecorder.current.onstop = async () => {
+        const blob = new Blob(audioChunks.current, { type: "audio/webm" });
+        const arrayBuffer = await blob.arrayBuffer();
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+        const wavArrayBuffer = toWav(audioBuffer);
+        const wavBlob = new Blob([wavArrayBuffer], { type: 'audio/wav' });
+
+        try {
+          await sendAudioToAPI(wavBlob, 'recorded-audio.wav');
+        } catch (error) {
+          console.error(error);
+        }
+
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "recorded-audio.webm";
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+
+
+        // Limpiar la grabación y los chunks
+        mediaRecorder.current = null;
+        audioChunks.current = [];
+      };
+    }
+  };
+
+  const sendAudioToAPI = async (audioBlob, fileName) => {
+    const formData = new FormData();
+    formData.append('audio', audioBlob, fileName);
+  
+    try {
+      const response = await fetch('http://localhost:5000/transcribe', {
+        method: 'POST',
+        
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al enviar el audio a la API');
+      }
+  
+      const data = await response.json();
+  
+      // Manejar la respuesta JSON
+      console.log(data); // Imprimir la respuesta JSON en la consola
+  
+      // Aquí puedes agregar más lógica para trabajar con los datos, por ejemplo, actualizar el estado de tu componente React
+    } catch (error) {
+      console.error(error);
+      // Manejar errores de solicitud
     }
   };
 
   return (
     <div>
-      <h2>Grabación de Audio</h2>
-      <button onClick={startRecording} disabled={recording}>
-        Iniciar Grabación
+      <button
+        onMouseDown={startRecording}
+        onMouseUp={stopRecordingAndDownload}
+        onTouchStart={startRecording}
+        onTouchEnd={stopRecordingAndDownload}
+      >
+        Microfono
       </button>
-      <button onClick={stopRecording} disabled={!recording}>
-        Detener Grabación
-      </button>
-      {audioUrl && <audio controls src={audioUrl}></audio>}
     </div>
   );
-}
+};
 
 export default AudioRecorder;
