12654cbe30adff29f84ad0b6aea9b17b045006e5
JuniorAlfredoHuerta <44213192+JuniorAlfredoHuerta@users.noreply.github.com>
2024-05-04 07:23:28 -0500

    update
    asd

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,6 @@ import Cookies from "js-cookie";
 console.log(Cookies.get());
 
 const instance = axios.create({
-  //baseURL: "http://localhost:7000/api",
   baseURL: "https://backend-production-a138.up.railway.app/api",
   headers: {
     Authorization: Cookies.get()?.token,
