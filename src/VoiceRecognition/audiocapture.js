013f0db5d8e645f202b274ee23a31eb6186025bd
JuniorAlfredoHuerta <44213192+JuniorAlfredoHuerta@users.noreply.github.com>
2024-06-14 18:22:21 -0500

    missing one error
    cambiar puerto por error.

--- src/VoiceRecognition/audiocapture.js
+++ src/VoiceRecognition/audiocapture.js
@@ -106,8 +106,8 @@ const AudioRecorder = ({ onApiResponse, onError }) => {
 
     try {
       const response = await fetch(
-        //"https://apienv-production.up.railway.app/transcribe",
-        "http://localhost:5000/transcribe",
+        "https://apienv-production.up.railway.app/transcribe",
+        //"http://localhost:5000/transcribe",
 
         {
           method: "POST",
